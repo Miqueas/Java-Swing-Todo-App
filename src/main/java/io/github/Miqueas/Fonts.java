9d14cf747c81088ecb0ec42ea69771072f82707d
Josué Martínez <JosueTech@proton.me>
2024-11-30 18:47:53 -0500

    Created fonts class

---
+++ src/main/java/io/github/Miqueas/Fonts.java
@@ -0,0 +1,10 @@
+package io.github.Miqueas;
+
+import java.awt.Font;
+
+public class Fonts {
+  public static Font normal = new Font("sans serif", Font.PLAIN, 12);
+  public static Font bold = new Font("sans serif", Font.BOLD, 12);
+  public static Font title = new Font("sans serif", Font.BOLD, 16);
+  public static Font display = new Font("sans serif", Font.BOLD, 20);
+}
