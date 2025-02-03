43da9489e28b2db67f67c977fdf6ca7ee01c59fb
Josué Martínez <JosueTech@proton.me>
2025-02-03 12:42:06 -0500

    Changes in Main.java:
    - Added some MacOS tweaks
    - Added some Linux tweaks

--- app/src/main/java/io/github/Miqueas/JavaSwingTodoApp/Main.java
+++ app/src/main/java/io/github/Miqueas/JavaSwingTodoApp/Main.java
@@ -1,13 +1,28 @@
 package io.github.Miqueas.JavaSwingTodoApp;
 
-import javax.swing.*;
-
+import com.formdev.flatlaf.fonts.inter.FlatInterFont;
 import com.formdev.flatlaf.themes.FlatMacDarkLaf;
+import com.formdev.flatlaf.util.SystemInfo;
+import javax.swing.*;
 
 public class Main {
   public static void main(String[] args) {
-    FlatMacDarkLaf.setup();
+    // Integrates the app menu bar with the system menu bar on macOS
+    if (SystemInfo.isMacOS) {
+      System.setProperty("apple.laf.useScreenMenuBar", "true");
+      System.setProperty("apple.awt.application.name", "Java Swing Todo App");
+      System.setProperty("apple.awt.application.appearance", "system");
+    }
+
+    // Enable custom window decorations on Linux
+    if (SystemInfo.isLinux) {
+      JFrame.setDefaultLookAndFeelDecorated(true);
+      JDialog.setDefaultLookAndFeelDecorated(true);
+    }
+
     SwingUtilities.invokeLater(() -> {
+      FlatMacDarkLaf.setup();
+      FlatInterFont.installLazy();
       AppWindow window = new AppWindow();
       window.setVisible(true);
     });
